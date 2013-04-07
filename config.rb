11da2eb82c3cecc9a546e96f57938db6f7e3a61e
Derek Hubbard <derek@derekhubbard.com>
2013-04-07 00:21:35 -0400

    Adding initial config.

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/derekhubbard/"
+http_images_path = "/derekhubbard/images"
+http_fonts_path = "/derekhubbard/fonts"
+css_dir = "public/derekhubbard/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
